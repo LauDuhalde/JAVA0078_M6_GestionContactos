4275d31d30e45b31bd1d60b2f94157bcda428dd4
LauDuhalde <duhalde.ulloa.laura@gmail.com>
2025-10-24 21:25:32 -0300

    lombok

--- src/main/java/cl/web/model/Contacto.java
+++ src/main/java/cl/web/model/Contacto.java
@@ -2,45 +2,17 @@ package cl.web.model;
 
 import org.springframework.stereotype.Component;
 
-@Component //le dice a Spring que esta clase es un bean gestionado, es decir, puede ser inyectada con @Autowired
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Component //para que se pueda registrar en el contexto de spring.
+@Data //para que se puedan usar los getters y setters.
+@NoArgsConstructor //para que se puedan crear objetos sin atributos.
+@AllArgsConstructor //para que se puedan crear objetos con todos sus atributos.
 public class Contacto {
+
     private String nombre;
     private String correo;
     private String telefono;
-
-    public Contacto() {
-    	
-    }
-
-    public Contacto(String nombre, String correo, String telefono) {
-        this.nombre = nombre;
-        this.correo = correo;
-        this.telefono = telefono;
-    }
-
-	public String getNombre() {
-		return nombre;
-	}
-
-	public void setNombre(String nombre) {
-		this.nombre = nombre;
-	}
-
-	public String getCorreo() {
-		return correo;
-	}
-
-	public void setCorreo(String correo) {
-		this.correo = correo;
-	}
-
-	public String getTelefono() {
-		return telefono;
-	}
-
-	public void setTelefono(String telefono) {
-		this.telefono = telefono;
-	}
-    
-    
 }
